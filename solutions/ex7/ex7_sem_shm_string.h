c06fc48a0f8fc2cc90afd6593484daebfcc6cbdb
Capobanco <cccapobianchi@gmail.com>
2023-07-19 15:37:50 +0200

    Update ex7_sem_shm_string.h

--- solutions/ex7/ex7_sem_shm_string.h
+++ solutions/ex7/ex7_sem_shm_string.h
@@ -1,8 +1,3 @@
-/* ex7_sem_shm_string.h
-
-    Licensed under GNU General Public License v2 or later.
-*/
-
 #define EMAIL_LENGTH 64+1 //+1 for trailing '\0'
 #define HASH_LENGTH 64+1
 #define MSG_SIZE 64+1
